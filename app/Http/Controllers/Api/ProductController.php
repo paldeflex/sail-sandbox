447400ad428c70f457c9d76b5a5be2565a34e2fe
paldeflex <paldeflex@gmail.com>
2024-10-02 21:22:10 +0500

    Заготовка для продукта

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return Product::all();
+    }
+
+    public function store(StoreProductRequest $request)
+    {
+        //
+    }
+
+    public function show(Product $product)
+    {
+        //
+    }
+
+    public function update(UpdateProductRequest $request, Product $product)
+    {
+        //
+    }
+
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
