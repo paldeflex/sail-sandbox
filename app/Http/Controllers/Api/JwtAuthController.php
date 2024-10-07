97f959ca16877d1033a05c57c72826b6a9bc9c9b
paldeflex <paldeflex@gmail.com>
2024-10-07 11:43:48 +0500

    Починил регистрацию

--- app/Http/Controllers/Api/JwtAuthController.php
+++ app/Http/Controllers/Api/JwtAuthController.php
@@ -6,9 +6,8 @@ use App\DTO\LoginDTO;
 use App\DTO\RegisterDTO;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
 use App\Services\UserService;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
 
 class JwtAuthController extends Controller
@@ -20,18 +19,8 @@ class JwtAuthController extends Controller
         $this->userService = $userService;
     }
 
-    public function register(Request $request)
+    public function register(RegisterRequest $request)
     {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6',
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json($validator->errors()->toJson(), Response::HTTP_BAD_REQUEST);
-        }
-
         $registerDTO = new RegisterDTO(
             $request->input('name'),
             $request->input('email'),
